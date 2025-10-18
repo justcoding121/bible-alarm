274f6c169f38120b49efd45fa62f6b9138dd1565
Jehonathan Thomas <jehonathant@gmail.com>
2025-10-18 17:35:09 -0500

    rm duplicates

--- src/Bible.Alarm/Common/Helpers/UrlHelper.cs
+++
@@ -1,8 +0,0 @@
-﻿using Bible.Alarm.Shared.Constants;
-
-namespace Bible.Alarm.Common.Helpers;
-
-public class UrlHelper
-{
-    public static string JwOrgIndexServiceBaseUrl = AppConstants.ApiEndpoints.JwOrgIndexServiceBaseUrl;
-}
