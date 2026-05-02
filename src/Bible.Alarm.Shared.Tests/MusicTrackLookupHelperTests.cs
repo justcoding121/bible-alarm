171448e22808419301a3f7276aa92a6b5435cd45
Jehonathan Thomas <jehonathant@gmail.com>
2026-05-01 20:12:12 -0500

    test(Shared): cover MusicTrackLookupHelper TryGet/GetKey by code

---
+++ src/Bible.Alarm.Shared.Tests/MusicTrackLookupHelperTests.cs
@@ -0,0 +1,65 @@
+#nullable enable
+
+using Bible.Alarm.Shared.Helpers;
+using Bible.Alarm.Shared.Models.Media.Music;
+
+namespace Bible.Alarm.Shared.Tests;
+
+public sealed class MusicTrackLookupHelperTests
+{
+    private static Dictionary<int, MusicTrack> Tracks(params (int Key, string Code, string Title)[] items)
+    {
+        var dict = new Dictionary<int, MusicTrack>();
+        foreach (var (key, code, title) in items)
+        {
+            dict[key] = new MusicTrack { TrackCode = code, Title = title };
+        }
+
+        return dict;
+    }
+
+    [Fact]
+    public void TryGetByCode_ReturnsFalse_WhenNullOrBlankCodeOrEmptyDictionary()
+    {
+        var tracks = Tracks((0, "1", "A"));
+
+        Assert.False(MusicTrackLookupHelper.TryGetByCode(tracks, (string?)null, out _));
+        Assert.False(MusicTrackLookupHelper.TryGetByCode(tracks, "", out _));
+        Assert.False(MusicTrackLookupHelper.TryGetByCode(tracks, "   ", out _));
+        Assert.False(MusicTrackLookupHelper.TryGetByCode(new Dictionary<int, MusicTrack>(), "1", out _));
+    }
+
+    [Fact]
+    public void TryGetByCode_ReturnsFirstMatch_UsingNumericCodeEquality()
+    {
+        var tracks = Tracks((10, "2", "Two"), (20, "01", "One"));
+
+        Assert.True(MusicTrackLookupHelper.TryGetByCode(tracks, "1", out var one));
+        Assert.Equal(20, one.Key);
+        Assert.Equal("01", one.Track.TrackCode);
+
+        Assert.True(MusicTrackLookupHelper.TryGetByCode(tracks, "002", out var two));
+        Assert.Equal(10, two.Key);
+
+        Assert.False(MusicTrackLookupHelper.TryGetByCode(tracks, "3", out _));
+    }
+
+    [Fact]
+    public void TryGetByCode_MatchesAlphanumeric_WithOrdinalIgnoreCase()
+    {
+        var tracks = Tracks((1, "jwb-tag", "T"));
+
+        Assert.True(MusicTrackLookupHelper.TryGetByCode(tracks, "JwB-TaG", out var hit));
+        Assert.Equal(1, hit.Key);
+    }
+
+    [Fact]
+    public void GetKeyByCode_ReturnsKeyOrNull_WrappingTryGet()
+    {
+        var tracks = Tracks((5, "10", "X"));
+
+        Assert.Equal(5, MusicTrackLookupHelper.GetKeyByCode(tracks, "010"));
+        Assert.Null(MusicTrackLookupHelper.GetKeyByCode(tracks, ""));
+        Assert.Null(MusicTrackLookupHelper.GetKeyByCode(tracks, "z"));
+    }
+}
