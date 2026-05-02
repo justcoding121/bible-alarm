e5f8a85110539246f4791abca9cf3d0b02fc34b3
Jehonathan Thomas <jehonathant@gmail.com>
2026-05-02 07:43:50 -0500

    test(Bible.Alarm.Tests): cover SafeTeardown and DisplayNameNormalizer
    SafeTeardown: cancel/dispose paths including disposed CTS and double teardown. DisplayNameNormalizer: null/whitespace, HTML decode + trim, NBSP collapsing to empty.

---
+++ src/Bible.Alarm.Tests/DisplayNameNormalizerTests.cs
@@ -0,0 +1,35 @@
+#nullable enable
+
+using Bible.Alarm.Services.Schedule.ScheduleDisplayNameServiceHelpers;
+
+namespace Bible.Alarm.Tests;
+
+public sealed class DisplayNameNormalizerTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public void NormalizeTrackTitle_returns_null_for_missing_or_whitespace(string? raw)
+    {
+        Assert.Null(DisplayNameNormalizer.NormalizeTrackTitle(raw));
+    }
+
+    [Fact]
+    public void NormalizeTrackTitle_trims_and_decodes_entities()
+    {
+        Assert.Equal("A & B", DisplayNameNormalizer.NormalizeTrackTitle("  A &amp; B  "));
+    }
+
+    [Fact]
+    public void NormalizeTrackTitle_replaces_nbsp_then_returns_null_when_only_spaces_remain()
+    {
+        Assert.Null(DisplayNameNormalizer.NormalizeTrackTitle("\u00A0\u00A0"));
+    }
+
+    [Fact]
+    public void NormalizeTrackTitle_returns_null_when_decode_yields_empty_after_trim()
+    {
+        Assert.Null(DisplayNameNormalizer.NormalizeTrackTitle("\u00A0"));
+    }
+}
---
+++ src/Bible.Alarm.Tests/SafeTeardownTests.cs
@@ -0,0 +1,64 @@
+#nullable enable
+
+using Bible.Alarm.Common.Helpers;
+
+namespace Bible.Alarm.Tests;
+
+public sealed class SafeTeardownTests
+{
+    [Fact]
+    public async Task CancelAsyncNoThrow_completes_when_source_active()
+    {
+        using var cts = new CancellationTokenSource();
+
+        await SafeTeardown.CancelAsyncNoThrow(cts);
+
+        Assert.True(cts.IsCancellationRequested);
+    }
+
+    [Fact]
+    public async Task CancelAsyncNoThrow_does_not_throw_when_already_disposed()
+    {
+        var cts = new CancellationTokenSource();
+        cts.Dispose();
+
+        await SafeTeardown.CancelAsyncNoThrow(cts);
+    }
+
+    [Fact]
+    public void CancelDisposeNoThrow_null_is_no_op()
+    {
+        SafeTeardown.CancelDisposeNoThrow(null);
+    }
+
+    [Fact]
+    public void CancelDisposeNoThrow_cancels_and_disposes()
+    {
+        var cts = new CancellationTokenSource();
+
+        SafeTeardown.CancelDisposeNoThrow(cts);
+
+        Assert.True(cts.IsCancellationRequested);
+        Assert.Throws<ObjectDisposedException>(() => cts.Cancel());
+    }
+
+    [Fact]
+    public void CancelDisposeNoThrow_when_already_cancelled_still_disposes()
+    {
+        var cts = new CancellationTokenSource();
+        cts.Cancel();
+
+        SafeTeardown.CancelDisposeNoThrow(cts);
+
+        Assert.Throws<ObjectDisposedException>(() => cts.Cancel());
+    }
+
+    [Fact]
+    public void CancelDisposeNoThrow_twice_does_not_throw()
+    {
+        var cts = new CancellationTokenSource();
+
+        SafeTeardown.CancelDisposeNoThrow(cts);
+        SafeTeardown.CancelDisposeNoThrow(cts);
+    }
+}
