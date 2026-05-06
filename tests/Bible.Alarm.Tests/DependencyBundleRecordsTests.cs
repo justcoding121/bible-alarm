814d04ecceb1a1719d83e2a7d6ca66f4330c1d01
Jehonathan Thomas <jehonathant@gmail.com>
2026-05-05 21:48:10 -0500

    test: extend dependency bundle coverage (playback, playlist, music state)
    Cover PlaybackServiceInjectionContext, PlaylistServiceDeps,
    ScheduleListItemViewModelDeps, music state change handler service/collaborator
    records, MusicStateHolder, and scheduler load-args records.

--- tests/Bible.Alarm.Tests/DependencyBundleRecordsTests.cs
+++ tests/Bible.Alarm.Tests/DependencyBundleRecordsTests.cs
@@ -3,10 +3,14 @@
 using AutoMapper;
 using Bible.Alarm.Services.Media;
 using Bible.Alarm.Services.Media.AudioPlayerHelpers;
+using Bible.Alarm.Services.Media.Playback;
+using Bible.Alarm.Services.Scheduler;
+using Bible.Alarm.Tests.Support;
 using Bible.Alarm.ViewModels;
 using Bible.Alarm.ViewModels.HomeViewModelHelpers;
+using Bible.Alarm.ViewModels.Schedule.MusicSelectionContainer;
+using Bible.Alarm.ViewModels.ScheduleViewModelHelpers.MusicSelection;
 using Bible.Alarm.ViewModels.Shared;
-using Bible.Alarm.Tests.Support;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
 
@@ -114,5 +118,101 @@ public sealed class DependencyBundleRecordsTests
         Assert.Equal(TimeSpan.FromSeconds(2), sut.GetDuration());
         Assert.Null(sut.GetMediaOpenedCompletionSource());
         Assert.Null(sut.GetCurrentTrack());
+    }
+
+    [Fact]
+    public void PlaybackServiceInjectionContext_primary_ctor_round_trips()
+    {
+        var sut = new PlaybackServiceInjectionContext(
+            null!, null!, null!, null!, null!, null!, null!, null!);
+
+        Assert.Null(sut.PlaylistService);
+        Assert.Null(sut.DefaultDeviceRingtoneService);
+    }
+
+    [Fact]
+    public void PlaylistServiceDeps_primary_ctor_round_trips()
+    {
+        var sut = new PlaylistServiceDeps(
+            null!, null!, null!, null!, null!, null!, null!, null!, null!);
+
+        Assert.Null(sut.Logger);
+        Assert.Null(sut.ScheduleDisplayNameService);
+    }
+
+    [Fact]
+    public void ScheduleListItemViewModelDeps_primary_ctor_round_trips()
+    {
+        var sut = new ScheduleListItemViewModelDeps(
+            null!, null!, null!, null!, null!, null!, null!, null!, null!);
+
+        Assert.Null(sut.CategoryNameService);
+        Assert.Null(sut.ScheduleStateService);
+    }
+
+    [Fact]
+    public void MusicStateChangeHandlerServices_primary_ctor_round_trips()
+    {
+        var sut = new MusicStateChangeHandlerServices(null!, null!, null!, null!, null!);
+
+        Assert.Null(sut.Mapper);
+        Assert.Null(sut.ServiceProvider);
+    }
+
+    [Fact]
+    public void MusicStateChangeHandlerCollaborators_primary_ctor_round_trips()
+    {
+        var sut = new MusicStateChangeHandlerCollaborators(null!, null!, null!);
+
+        Assert.Null(sut.StateTracker);
+        Assert.Null(sut.DisplayTextProvider);
+    }
+
+    [Fact]
+    public void MusicStateHolder_exposes_mutable_music_flags()
+    {
+        var sut = new MusicStateHolder
+        {
+            MusicUpdated = true,
+            IsUpdatingFromState = true,
+            PendingMusicEnabled = false,
+        };
+
+        Assert.True(sut.MusicUpdated);
+        Assert.True(sut.IsUpdatingFromState);
+        Assert.False(sut.PendingMusicEnabled!.Value);
+    }
+
+    [Fact]
+    public void LoadMusicForSelectionArgs_round_trips()
+    {
+        var sut = new LoadMusicForSelectionArgs(3, true, null, "p", "lc", "tc", false);
+
+        Assert.Equal(3, sut.ScheduleId);
+        Assert.True(sut.IsNewSchedule);
+        Assert.Equal("p", sut.PublicationCode);
+        Assert.Equal("lc", sut.LanguageCode);
+        Assert.Equal("tc", sut.TrackCode);
+        Assert.False(sut.Repeat!.Value);
+    }
+
+    [Fact]
+    public void LoadBiblePublicationScheduleCodes_round_trips()
+    {
+        var sut = new LoadBiblePublicationScheduleCodes("en", "pub", "sec", "trk");
+
+        Assert.Equal("en", sut.LanguageCode);
+        Assert.Equal("sec", sut.SectionCode);
+    }
+
+    [Fact]
+    public void LoadBiblePublicationForSelectionArgs_round_trips()
+    {
+        var codes = new LoadBiblePublicationScheduleCodes(null, "pc", null, "tc");
+        var sut = new LoadBiblePublicationForSelectionArgs(9, false, null, codes, TimeSpan.FromMinutes(2));
+
+        Assert.Equal(9, sut.ScheduleId);
+        Assert.Same(codes, sut.Codes);
+        Assert.Equal(TimeSpan.FromMinutes(2), sut.FinishedDuration);
     }
 }
