0236bce05bc797cd71f204f4a8b203b0c4eff3fa
Jehonathan Thomas <jehonathant@gmail.com>
2026-05-05 21:59:37 -0500

    test: cover more playback request DTOs and Fluxor schedule actions
    Add tests for PlaybackPrepareFallbackRequest, PlaybackHandleFailureRequest,
    PlaybackMediaEndedRequest, playback transition actions, TrackSelectionAction,
    draft/last-played/view-existing schedule actions, and InitializeAction
    (null vs explicit list).

--- tests/Bible.Alarm.Tests/DependencyBundleRecordsTests.cs
+++ tests/Bible.Alarm.Tests/DependencyBundleRecordsTests.cs
@@ -8,8 +8,14 @@ using Bible.Alarm.Services.Media;
 using Bible.Alarm.Services.Media.AudioPlayerHelpers;
 using Bible.Alarm.Services.Media.Playback;
 using Bible.Alarm.Services.Scheduler;
+using Bible.Alarm.Shared.DataStructures;
 using Bible.Alarm.Shared.Models.Media;
+using Bible.Alarm.Stores.Actions;
+using Bible.Alarm.Stores.Actions.BiblePublications;
+using Bible.Alarm.Stores.Actions.Playback;
+using Bible.Alarm.Stores.Actions.Schedule;
 using Bible.Alarm.Stores.Effects;
+using Bible.Alarm.Stores.Models;
 using Bible.Alarm.Tests.Support;
 using Bible.Alarm.ViewModels;
 using Bible.Alarm.ViewModels.BiblePublications;
@@ -428,5 +434,137 @@ public sealed class DependencyBundleRecordsTests
         Assert.True(timerStopped);
         Assert.True(sut.SkipMarkAsPlayed);
         Assert.Same(meta, sut.TrackMetadataToMark);
+    }
+
+    [Fact]
+    public void PlaybackPrepareFallbackRequest_SetPlaylist_invokes_delegate()
+    {
+        var setPlaylistCalls = 0;
+        var sut = new PlaybackPrepareFallbackRequest(
+            42,
+            false,
+            _ => setPlaylistCalls++,
+            _ => { },
+            () => { },
+            (_, _) => { },
+            _ => Task.CompletedTask);
+
+        sut.SetPlaylist([]);
+
+        Assert.Equal(1, setPlaylistCalls);
+        Assert.Equal(42, sut.ScheduleId);
+    }
+
+    [Fact]
+    public async Task PlaybackHandleFailureRequest_ResetAsync_invokes()
+    {
+        var resetCalls = 0;
+        var sut = new PlaybackHandleFailureRequest(
+            true,
+            8,
+            () =>
+            {
+                resetCalls++;
+                return Task.CompletedTask;
+            },
+            _ => { },
+            _ => { },
+            _ => Task.CompletedTask);
+
+        await sut.ResetAsync();
+
+        Assert.Equal(1, resetCalls);
+        Assert.True(sut.IsAlarm);
+        Assert.Equal(8, sut.CurrentScheduleId);
+    }
+
+    [Fact]
+    public void PlaybackMediaEndedRequest_GetCurrentTrackIndex_round_trips()
+    {
+        var sut = new PlaybackMediaEndedRequest(
+            null,
+            () => 11,
+            _ => { },
+            3,
+            true,
+            () => Task.FromResult(false),
+            _ => Task.CompletedTask,
+            _ => Task.CompletedTask,
+            () => false,
+            () => true,
+            (_, _) => Task.CompletedTask);
+
+        Assert.Equal(11, sut.GetCurrentTrackIndex());
+        Assert.Equal(3, sut.CurrentScheduleId);
+        Assert.True(sut.IsIndefinitePlayback);
+    }
+
+    [Fact]
+    public void PlaybackTrackTransitionEndedAction_instantiates()
+    {
+        var sut = new PlaybackTrackTransitionEndedAction();
+        Assert.NotNull(sut);
+    }
+
+    [Fact]
+    public void PlaybackTrackTransitionStartedAction_instantiates()
+    {
+        var sut = new PlaybackTrackTransitionStartedAction();
+        Assert.NotNull(sut);
+    }
+
+    [Fact]
+    public void TrackSelectionAction_exposes_state_item()
+    {
+        var item = new BiblePublicationStateItem { Id = 77, PublicationCode = "p" };
+        var sut = new TrackSelectionAction(item);
+
+        Assert.Same(item, sut.CurrentBiblePublicationSchedule);
+    }
+
+    [Fact]
+    public void UpdateDraftScheduleAction_exposes_schedule()
+    {
+        var sched = new ScheduleStateItem { Id = 5, Name = "n" };
+        var sut = new UpdateDraftScheduleAction(sched);
+
+        Assert.Same(sched, sut.Schedule);
+    }
+
+    [Fact]
+    public void UpdateScheduleLastPlayedAction_exposes_fields()
+    {
+        var t = new DateTime(2024, 6, 1, 12, 0, 0, DateTimeKind.Utc);
+        var sut = new UpdateScheduleLastPlayedAction(99, t);
+
+        Assert.Equal(99, sut.ScheduleId);
+        Assert.Equal(t, sut.LastPlayedAtUtc);
+    }
+
+    [Fact]
+    public void ViewExistingScheduleAction_exposes_fields()
+    {
+        var sut = new ViewExistingScheduleAction(12, isEnabled: false);
+
+        Assert.Equal(12, sut.ScheduleId);
+        Assert.False(sut.IsEnabled);
+    }
+
+    [Fact]
+    public void InitializeAction_null_scheduleList_uses_empty_hash_set()
+    {
+        var sut = new InitializeAction(null!);
+
+        Assert.NotNull(sut.ScheduleList);
+        Assert.Empty(sut.ScheduleList);
+    }
+
+    [Fact]
+    public void InitializeAction_preserves_passed_schedule_list()
+    {
+        var list = new ObservableHashSet<ScheduleStateItem>();
+        var sut = new InitializeAction(list);
+
+        Assert.Same(list, sut.ScheduleList);
     }
 }
