31b9e820dc6500f8cf627f593c85d77de90d2d29
Jehonathan Thomas <jehonathant@gmail.com>
2026-05-07 00:21:50 -0500

    test(Shared): cover AppConstants ApiEndpoints, Database, ReviewSettings

--- tests/Bible.Alarm.Shared.Tests/Constants/AppConstantsTests.cs
+++ tests/Bible.Alarm.Shared.Tests/Constants/AppConstantsTests.cs
@@ -23,4 +23,69 @@ public sealed class AppConstantsTests
         Assert.NotEmpty(AppConstants.Media.GetPubSingleTrackNoParamPublicationCodes);
         Assert.NotEmpty(AppConstants.Media.GetPubSingleTrackZeroPublicationCodes);
     }
+
+    [Fact]
+    public void ApiEndpoints_mirror_hosts_and_GETPUBMediator_base_urls_are_well_formed()
+    {
+        Assert.Equal("b.jw-cdn.org", AppConstants.ApiEndpoints.JwCdnHostB);
+        Assert.Equal("app.jw-cdn.org", AppConstants.ApiEndpoints.JwCdnHostApp);
+
+        foreach (var origin in new[] { AppConstants.ApiEndpoints.JwCdnOriginHttpsB, AppConstants.ApiEndpoints.JwCdnOriginHttpsApp })
+        {
+            Assert.StartsWith("https://", origin, StringComparison.Ordinal);
+        }
+
+        Assert.Equal(
+            AppConstants.ApiEndpoints.JwCdnOriginHttpsB + AppConstants.ApiEndpoints.PubMediaApisGetPubMedialinksPath,
+            AppConstants.ApiEndpoints.JwOrgIndexServiceBaseUrl);
+
+        Assert.Equal(2, AppConstants.ApiEndpoints.JwOrgIndexServiceBaseUrls.Length);
+        Assert.All(
+            AppConstants.ApiEndpoints.JwOrgIndexServiceBaseUrls,
+            u => Assert.Contains("GETPUBMEDIALINKS", u, StringComparison.OrdinalIgnoreCase));
+
+        Assert.EndsWith(
+            AppConstants.ApiEndpoints.MediatorApisV1Path,
+            AppConstants.ApiEndpoints.JwOrgMediatorApiBaseUrl,
+            StringComparison.Ordinal);
+
+        Assert.Equal(2, AppConstants.ApiEndpoints.JwOrgMediatorApiBaseUrls.Length);
+        Assert.All(
+            AppConstants.ApiEndpoints.JwOrgMediatorApiBaseUrls,
+            u => Assert.Contains("/apis/mediator/v1", u, StringComparison.Ordinal));
+
+        Assert.Equal(
+            AppConstants.ApiEndpoints.JwOrgPublicWebsiteHttpsOrigin + AppConstants.ApiEndpoints.JwOrgLanguagesListPath,
+            AppConstants.ApiEndpoints.JwOrgLanguagesListUrl);
+    }
+
+    [Fact]
+    public void Database_sqlite_auxiliary_suffix_list_and_schedule_media_formats()
+    {
+        Assert.Equal(
+            new[]
+            {
+                AppConstants.Database.SqliteWalFileSuffix,
+                AppConstants.Database.SqliteShmFileSuffix,
+                AppConstants.Database.SqliteJournalFileSuffix,
+            },
+            AppConstants.Database.SqliteAuxiliaryFileSuffixes);
+
+        Assert.Equal("Filename={0}", AppConstants.Database.ScheduleDatabaseConnectionStringFormat);
+        Assert.Equal("Filename={0}", AppConstants.Database.MediaIndexDatabaseConnectionStringFormat);
+        Assert.Equal("schedule.db", AppConstants.Database.ScheduleDatabaseFileName);
+        Assert.Equal("mediaIndex.db", AppConstants.Database.MediaIndexDatabaseFileName);
+    }
+
+    [Fact]
+    public void ReviewSettings_gate_values_match_product_expectations()
+    {
+        Assert.Equal(7, AppConstants.ReviewSettings.MinimumAppOpens);
+        Assert.Equal(10, AppConstants.ReviewSettings.MinimumDismissals);
+        Assert.Equal(7, AppConstants.ReviewSettings.MinimumDaysSinceFirstDismissal);
+        Assert.Equal(7, AppConstants.ReviewSettings.MinimumDaysSinceInstall);
+        Assert.Equal(7, AppConstants.ReviewSettings.MinimumDaysSinceFirstOpen);
+        Assert.Equal(30, AppConstants.ReviewSettings.RetryAfterDaysWhenNotFinalized);
+        Assert.Equal(5, AppConstants.ReviewSettings.MinimumMinutesBetweenCountedAppOpens);
+    }
 }
